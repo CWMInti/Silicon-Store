125c28728399243912f49bb968119dc21cef15d2
ChungWeiMing <82583267+ChungWeiMing@users.noreply.github.com>
2021-07-07 19:31:04 +0800

    Delete js directory

--- js/feedback_form.js
+++
@@ -1,6 +0,0 @@
-function collectFeedback() {
-	var feedTitle = document.getElementById("feedback_title").value;
-	var feedContent = document.getElementById("feedback_content").value;
-	
-	var feedForm = "Title: " + feedTitle + " | Content: " + feedContent;
-}
